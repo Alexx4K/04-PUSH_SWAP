2c2f5e5d074615394f77cab75a3728ccfa4f8ad6
CesarCRP97 <32871629+CesarCRP97@users.noreply.github.com>
2026-07-18 13:19:16 +0200

    add annotations to linked lists' functions

--- linked_lists.c
+++ linked_lists.c
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   linked_lists.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/02 12:22:01 by crubio-p          #+#    #+#             */
-/*   Updated: 2026/07/15 12:31:52 by crubio-p         ###   ########.fr       */
+/*   Updated: 2026/07/18 13:18:19 by cesar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "linked_lists.h"
 
+/// @brief Creates a node of t_list type.
+/// @param content The content in the node.
+/// @return The pointer to that t_list
 t_list	*ft_lstnew(int content)
 {
 	t_list	*new_node;
@@ -26,6 +29,9 @@ t_list	*ft_lstnew(int content)
 	return (new_node);
 }
 
+/// @brief Finds the last element in the list.
+/// @param lst
+/// @return A pointer tu the last element.
 t_list	*ft_lstlast(t_list *lst)
 {
 	if (lst == NULL)
@@ -35,11 +41,17 @@ t_list	*ft_lstlast(t_list *lst)
 	return (lst);
 }
 
+/// @brief If the stack is NULL, it means that it's empty
+/// @param stack
+/// @return 1 if empty, 0 if not.
 int	ft_is_empty(const t_list *stack)
 {
 	return (stack == NULL);
 }
 
+/// @brief Counts the number of elements in the list.
+/// @param lst
+/// @return The size of the list.
 int	ft_lstsize(t_list *lst)
 {
 	int	size;
